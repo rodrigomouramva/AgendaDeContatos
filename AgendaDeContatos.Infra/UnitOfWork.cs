f8206a1d2e24afdcadc25448783e50702d9a9926
Rodrigo Tadeu Alves de Moura <rodrigomoura.mva@outlook.com>
2017-04-13 09:16:18 -0300

    ffffffff

---
+++ AgendaDeContatos.Infra/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaDeContatos.Infra
+{
+    public class UnitOfWork<T> where T : DbContext
+    {
+        //protected  readonly T dbContext;
+        public T Context { get; private set; }
+        public UnitOfWork(T dbContext)
+        {
+            Context = dbContext;
+        }
+
+        public T Type { get; set; }
+
+        public void Save()
+        {
+
+        }
+    }
+}
